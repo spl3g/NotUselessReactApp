c32a84b8445ed7409104147292894d6951c45771
spl3g <spleefer6@yandex.ru>
2024-12-02 14:09:03 +0300

    feat: style service detail page

--- src/pages/service-detail-page/ServiceDetailPage.tsx
+++ src/pages/service-detail-page/ServiceDetailPage.tsx
@@ -5,6 +5,7 @@ import { getServiceAction } from "../../store/api-actions";
 import { Loader } from "@consta/uikit/Loader";
 import { Service } from "../../types/Services";
 import { Text } from "@consta/uikit/Text";
+import { Avatar } from "@consta/uikit/Avatar";
 
 const ServiceDetailPage = () => {
     const [service, setService] = useState<Service | null>(null);
@@ -14,17 +15,51 @@ const ServiceDetailPage = () => {
         getServiceAction(Number(id)).then((service) => {
             setService(service);
         });
-    }, []);
+    }, [id]);
 
     if (service === null) {
         return <Loader />;
     }
 
     return (
-        <>
-            <Text weight="semibold">{service.name}</Text>
-            <Text>{service.description}</Text>
-        </>
+        <div
+            style={{
+                display: "flex",
+                justifyContent: "space-around",
+            }}
+        >
+            <div style={{ display: "flex", gap: "1rem" }}>
+                <Avatar
+                    url={service.image}
+                    form="default"
+                    style={{
+                        width: "128px",
+                        height: "128px",
+                        margin: "auto 0",
+                        flexShrink: 0,
+                    }}
+                />
+                <div
+                    style={{
+                        display: "flex",
+                        justifyContent: "space-evenly",
+                        flexDirection: "column",
+                    }}
+                >
+                    <Text weight="semibold" size="3xl">
+                        {service.name}
+                    </Text>
+                    <Text size="l">{service.description}</Text>
+                    <Text
+                        align="right"
+                        view="secondary"
+                        style={{ marginTop: "auto" }}
+                    >
+                        {new Date(service.createdAt).toDateString()}
+                    </Text>
+                </div>
+            </div>
+        </div>
     );
 };
 
